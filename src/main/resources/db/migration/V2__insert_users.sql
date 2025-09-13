d6829eb3c2c0f0c7800da38f1715d21ace453e1f
Горецкий Николай Иванович <HoretskiNI@sapr.kbtem-omo.by>
2025-09-13 09:35:04 +0300

    edit V2__insert_users.sql

--- src/main/resources/db/migration/V2__insert_users.sql
+++ src/main/resources/db/migration/V2__insert_users.sql
@@ -5,7 +5,12 @@ VALUES ('admin', '$2a$12$OwzzwI7I8l1uo7vUD9Ff.eAGprZxaaQBq.qqW1nqvpegS2tU9yYQK',
 INSERT INTO users (username, password, email, firstname, lastname, enabled)
 VALUES ('user', '$2a$12$IhpxZSbUjjm4w1s0xmTDde/ad1LPeXnJBDLnAEcIQUIDV2ly1Lnom', 'nikolai.horetski@gmail.com', 'user', 'user', true);
 
+INSERT INTO users (username, password, email, firstname, lastname, enabled)
+VALUES ('test', '$2a$12$/KZBPArANUWJXMwAIe51F.8NEMYoLxZ4Ek0Qp/0AsvehxH8yL/WBi', 'nikolai.horetski@gmail.com', 'test', 'test', true);
+
 -- Вставка ролей для пользователей --
 INSERT INTO authority (username, authority) VALUES ('admin', 'ROLE_ADMIN');
 
 INSERT INTO authority (username, authority) VALUES ('user', 'ROLE_USER');
+
+INSERT INTO authority (username, authority) VALUES ('test', 'ROLE_USER');
